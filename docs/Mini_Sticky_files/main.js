1b135e2c6faee125fb4aaf044d58887ac059e541
Or Liran <orlir123@gmail.com>
2022-09-18 11:56:56 +0300

    Downloaded all game files from Armor Games

---
+++ docs/Mini_Sticky_files/main.js
@@ -0,0 +1,15 @@
+(function(){
+    Game.SINGLE_CONTROL = true;
+
+    Engine.Assets.root = "Package/";
+
+    Game.addAction("preloadchangecolor", function(){
+        Engine.Renderer.clearColor(0 / 255, 0 / 255, 0 / 255);
+        document.getElementById("gameDiv").style.backgroundColor = "black";
+    });
+    
+    Game.addAction("postinit", function(){
+        Game.SceneFade.setColor(0, 0, 0);
+    });
+
+})();
